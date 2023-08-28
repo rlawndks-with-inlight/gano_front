cd0a9efc1c4f5bf01e70a527639bfa74eb7d31bf
rlawndks4 <rlawndks0423@naver.com>
2023-08-29 01:07:56 +0900

    Feat: update

--- src/auth/AuthGuard.js
+++ src/auth/AuthGuard.js
@@ -5,7 +5,6 @@ import { useRouter } from 'next/router';
 // components
 import LoadingScreen from '../components/loading-screen';
 //
-import Login from '../pages/login';
 import { useAuthContext } from './useAuthContext';
 
 // ----------------------------------------------------------------------
@@ -15,31 +14,6 @@ AuthGuard.propTypes = {
 };
 
 export default function AuthGuard({ children }) {
-  const { isAuthenticated, isInitialized } = useAuthContext();
-
-  const { pathname, push } = useRouter();
-
-  const [requestedLocation, setRequestedLocation] = useState(null);
-
-  useEffect(() => {
-    if (requestedLocation && pathname !== requestedLocation) {
-      push(requestedLocation);
-    }
-    if (isAuthenticated) {
-      setRequestedLocation(null);
-    }
-  }, [isAuthenticated, pathname, push, requestedLocation]);
-
-  if (!isInitialized) {
-    return <LoadingScreen />;
-  }
-
-  if (!isAuthenticated) {
-    if (pathname !== requestedLocation) {
-      setRequestedLocation(pathname);
-    }
-    return <Login />;
-  }
 
   return <> {children} </>;
 }
